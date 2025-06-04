5c288c6a23edffc11e252e2a5a9c56a5c884ab60
user <sofusc@gmail.com>
2025-06-04 13:28:28 +0200

    Implemented semantic analysis for chapter 8

--- src/semantic_analyser.py
+++ src/semantic_analyser.py
@@ -20,11 +20,17 @@ class MapEntry(NamedTuple):
     from_current_block: bool
 
 class SemanticAnalyser:
+    label_counter = 0
 
     def make_temporary(self, name):
         unique_name = f"{name}.{SharedCounter.get_value()}"
         SharedCounter.increment()
         return unique_name
+    
+    def make_label(self, label_name): #TODO: this is identical to method in emitter, shared util class instead of counter?
+        label_name += str(self.label_counter)
+        self.label_counter += 1
+        return label_name
     
     def copy_variable_map(self, variable_map):
         result = copy.deepcopy(variable_map)
@@ -41,6 +47,13 @@ class SemanticAnalyser:
         if init is not None:
             init = self.resolve_exp(init, variable_map)
         return Declaration(unique_name, init)
+    
+    def resolve_for_init(self, init, variable_map):
+        match init:
+            case InitExp(exp):
+                return InitExp(self.resolve_exp(exp, variable_map))
+            case InitDecl(decl):
+                return InitDecl(self.resolve_declaration(decl, variable_map))
 
     def resolve_statement(self, statement, variable_map):
         match statement:
@@ -53,8 +66,25 @@ class SemanticAnalyser:
             case Compound(block):
                 new_variable_map = self.copy_variable_map(variable_map)
                 return Compound(self.resolve_block(block, new_variable_map))
+            case Break():
+                return Break()
+            case Continue():
+                return Continue()
+            case While(cond, body):
+                return While(self.resolve_exp(cond, variable_map), self.resolve_statement(body, variable_map))
+            case DoWhile(body, cond):
+                return DoWhile(self.resolve_statement(body, variable_map), self.resolve_exp(cond, variable_map))
+            case For(init, cond, post, body):
+                new_variable_map = self.copy_variable_map(variable_map)
+                init = self.resolve_for_init(init, new_variable_map)
+                cond = self.resolve_exp(cond, new_variable_map)
+                post = self.resolve_exp(post, new_variable_map)
+                body = self.resolve_statement(body, new_variable_map)
+                return For(init, cond, post, body)
             case Null():
                 return Null()
+            case _:
+                raise RuntimeError(f"Could not validate semantics for statement {statement}")
 
     def resolve_exp(self, exp, variable_map):
         match exp:
@@ -74,6 +104,10 @@ class SemanticAnalyser:
                 return Binary(binop, self.resolve_exp(left, variable_map), self.resolve_exp(right, variable_map))
             case Conditional(cond, then, else_):
                 return Conditional(self.resolve_exp(cond, variable_map), self.resolve_exp(then, variable_map), self.resolve_exp(else_, variable_map))
+            case None:
+                return None
+            case _:
+                raise RuntimeError(f"Could not validate semantics for expression {exp}")
     
     def resolve_block(self, block, variable_map):
         new_block_items = []
@@ -86,8 +120,46 @@ class SemanticAnalyser:
                 raise RuntimeError(f"Could not validate semantics for item {item}")
             new_block_items.append(item)
         return Block(new_block_items)
+    
+    def label_statement(self, statement, current_label = None):
+        match statement:
+            case Break():
+                if current_label is None:
+                    raise RuntimeError("Break statement outside loop")
+                return Break(current_label)
+            case Continue():
+                if current_label is None:
+                    raise RuntimeError("Continue statement outside loop")
+                return Continue(current_label)
+            case If(cond, then, else_):
+                return If(cond, self.label_statement(then, current_label), self.label_statement(else_, current_label) if else_ else None)
+            case Compound(block):
+                return Compound(self.label_block(block, current_label))
+            case While(cond, body):
+                new_label = self.make_label("loop")
+                labeled_body = self.label_statement(body, new_label)
+                return While(cond, labeled_body, new_label)
+            case DoWhile(body, cond):
+                new_label = self.make_label("loop")
+                labeled_body = self.label_statement(body, new_label)
+                return DoWhile(labeled_body, cond, new_label)
+            case For(init, cond, post, body):
+                new_label = self.make_label("loop")
+                labeled_body = self.label_statement(body, new_label)
+                return For(init, cond, post, labeled_body, new_label)
+            case _:
+                return statement
+    
+    def label_block(self, block, current_label = None):
+        new_block_items = []
+        for item in block.block_items:
+            if isinstance(item, Statement):
+                item = self.label_statement(item, current_label)
+            new_block_items.append(item)
+        return Block(new_block_items)
 
     def validate_program(self, program):
         variable_map: dict[str, MapEntry] = {}
         program.function.body = self.resolve_block(program.function.body, variable_map)
+        program.function.body = self.label_block(program.function.body)
         return program
